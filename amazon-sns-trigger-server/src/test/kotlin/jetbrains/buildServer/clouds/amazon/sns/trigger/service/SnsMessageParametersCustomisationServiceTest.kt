da48a313456c0e17b6c5b32708f8d4f9458ce2af
Andrei Efimov <andrei.efimov@jetbrains.com>
2023-01-20 17:26:57 +0100

    fix: tests fix

--- amazon-sns-trigger-server/src/test/kotlin/jetbrains/buildServer/clouds/amazon/sns/trigger/service/SnsMessageParametersCustomisationServiceTest.kt
+++ amazon-sns-trigger-server/src/test/kotlin/jetbrains/buildServer/clouds/amazon/sns/trigger/service/SnsMessageParametersCustomisationServiceTest.kt
@@ -6,8 +6,8 @@ import io.mockk.impl.annotations.RelaxedMockK
 import io.mockk.junit5.MockKExtension
 import jetbrains.buildServer.ExtensionHolder
 import jetbrains.buildServer.clouds.amazon.sns.trigger.utils.parameters.AwsSnsTriggerConstants
+import jetbrains.buildServer.serverSide.BuildPromotion
 import jetbrains.buildServer.serverSide.SBuild
-import jetbrains.buildServer.serverSide.TriggeredBy
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -22,19 +22,19 @@ internal class SnsMessageParametersCustomisationServiceTest {
     private lateinit var buildMock: SBuild
 
     @MockK
-    private lateinit var triggeredByMock: TriggeredBy
+    private lateinit var buildPromotion: BuildPromotion
 
     private lateinit var testable: SnsMessageParametersCustomisationService
 
     @BeforeEach
     fun setup() {
         testable = SnsMessageParametersCustomisationService(extensionHolderMock)
-        every { buildMock.triggeredBy } returns triggeredByMock
+        every { buildMock.buildPromotion } returns buildPromotion
     }
 
     @Test
     fun getParameters() {
-        every { triggeredByMock.parameters } returns mapOf(
+        every { buildPromotion.customParameters } returns mapOf(
             AwsSnsTriggerConstants.SNS_MESSAGE_SUBJECT_PARAMETER_PLACEHOLDER_KEY to "test subject",
             AwsSnsTriggerConstants.SNS_MESSAGE_BODY_PARAMETER_PLACEHOLDER_KEY to "test message",
             AwsSnsTriggerConstants.SNS_MESSAGE_ATTRIBUTES_PARAMETER_PLACEHOLDER_KEY_PREFIX + "foo" to "foo_value"
@@ -47,7 +47,7 @@ internal class SnsMessageParametersCustomisationServiceTest {
 
     @Test
     fun getParametersEmulation() {
-        every { triggeredByMock.parameters } returns emptyMap()
+        every { buildPromotion.customParameters } returns emptyMap()
         val result = testable.getParameters(buildMock, true)
         assertEquals("???", result[AwsSnsTriggerConstants.SNS_MESSAGE_SUBJECT_PARAMETER_PLACEHOLDER])
         assertEquals("???", result[AwsSnsTriggerConstants.SNS_MESSAGE_BODY_PARAMETER_PLACEHOLDER])
